9ec82b104e1babd5252bd3d1c1c6103785b07d90
Debasish Ray Chawdhuri <debasish.raychawdhuri@gmail.com>
2021-01-07 00:27:45 +0530

    add pom file

--- src/test/java/com/talentica/arangodb/SpringDataTestApplication.java
+++ src/test/java/com/talentica/arangodb/SpringDataTestApplication.java
@@ -4,16 +4,10 @@ import com.arangodb.springframework.config.ArangoConfiguration;
 import com.arangodb.springframework.core.ArangoOperations;
 import com.arangodb.springframework.core.convert.ArangoConverter;
 import com.arangodb.springframework.core.convert.ArangoEntityWriter;
-import com.arangodb.springframework.core.mapping.ArangoPersistentEntity;
-import com.arangodb.springframework.core.mapping.ArangoPersistentProperty;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mapping.context.MappingContext;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import java.io.File;
 
 @ComponentScan(basePackages = "com.talentica.arangodb")
 @EnableTransactionManagement
